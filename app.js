a5e33804e9a07e71ccda6d48b4b51234d296dab5
Fernando Pereira Lisboa <fernandopereiralisboa@gmail.com>
2018-02-06 11:12:39 -0200

    Added API-GateWay

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+var jwt = require('jsonwebtoken');
+const httpProxy = require('express-http-proxy')
+const app = express()
+
+const userServiceProxy = httpProxy('http://localhost:5000')
+
+// Authentication
+app.use((req, res, next) => {
+  let token = req.headers.authorization.split(" ");
+
+  var decoded = jwt.decode(token[1]);
+
+  req.headers['X-ViviPay-User-Id'] = decoded.sub;
+
+  next()
+})
+
+// Proxy request
+app.all('/*', (req, res, next) => {
+  userServiceProxy(req, res, next)
+})
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
